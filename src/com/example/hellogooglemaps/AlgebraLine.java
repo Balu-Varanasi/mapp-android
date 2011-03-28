d185a17f9965102e721c7b01c14a3db087b1c2ea
Joost Rijneveld <joostrijneveld@gmail.com>
2011-03-28 15:01:57 +0200

    Je kan nu mid-line een nieuw punt beginnen!

--- src/com/example/hellogooglemaps/AlgebraLine.java
+++ src/com/example/hellogooglemaps/AlgebraLine.java
@@ -21,7 +21,16 @@ public class AlgebraLine {
 	public AlgebraLine(Point start, Point end) {
 		//a = (Ay - By) / (Ax - Bx)
 		//b = Ay - (Ay - By) / (Ax - Bx) * Ax
-		this(((double)start.y - end.y) / (start.x - end.x), start.y - ((double)start.y - end.y) / (start.x - end.x)*start.x, start, end);
+		//this(((double)start.y - end.y) / (start.x - end.x), start.y - ((double)start.y - end.y) / (start.x - end.x)*start.x, start, end);
+		if (start.x <= end.x) {
+			this.start = start;
+			this.end = end;
+		}
+		else {
+			this.end = start;
+			this.start = end;
+		}
+		Log.v(HelloGoogleMaps.TAG, "Points: A: "+this.start.x+' '+this.start.y+" B: "+this.end.x+' '+this.end.y);
 	}
 	
 	/**
@@ -30,8 +39,15 @@ public class AlgebraLine {
 	public AlgebraLine(double a, double b, Point start, Point end) {
 		this.a = a;
 		this.b = b;
-		this.start = start;
-		this.end = end;
+		if (start.x <= end.x) {
+			this.start = start;
+			this.end = end;
+		}
+		else {
+			this.end = start;
+			this.start = end;
+		}
+		Log.v(HelloGoogleMaps.TAG, "Points: A: "+this.start.x+' '+this.start.y+" B: "+this.end.x+' '+this.end.y);
 	}
 	
 	/**
@@ -41,34 +57,11 @@ public class AlgebraLine {
 	 * @return true als het punt dichtbij genoeg ligt
 	 */
 	public boolean isNear(Point p, double d) {
-		double[] proj = projection(p, this.start);
-		if (start.x <= end.x) {
-			if (proj[0] < start.x)
-				proj[0] = start.x;
-			if (proj[0] > end.x)
-				proj[0] = end.x;
-		}
-		else {
-			if (proj[0] > start.x)
-				proj[0] = start.x;
-			if (proj[0] < end.x)
-				proj[0] = end.x;
-		}
-		if (start.y <= end.y) {
-			if (proj[1] < start.y)
-				proj[1] = start.y;
-			if (proj[1] > end.y)
-				proj[1] = end.y;
-		}
-		else {
-			if (proj[1] > start.y)
-				proj[1] = start.y;
-			if (proj[1] < end.y)
-				proj[1] = end.y;
-		}
-		Log.v(HelloGoogleMaps.TAG, "Projection: "+proj[0] + ' ' +proj[1]);
-		double distance = Math.sqrt(Math.pow(p.y - proj[1],2) + Math.pow(p.x - proj[0],2));
-		Log.v(HelloGoogleMaps.TAG, "Distance: "+distance);
+		//double[] proj = projection(p);
+		//Log.v(HelloGoogleMaps.TAG, "Projection: "+proj[0] + ' ' +proj[1]);
+		//double distance = distance(p, proj[0], proj[1]);
+		double distance = Math.abs((end.x - start.x) * (start.y - p.y) - (end.y - start.y) * (start.x - p.x)) / distance(start, end);
+		//Log.v(HelloGoogleMaps.TAG, "Distance: "+distance);
 		return distance <= d;
 	}
 
@@ -79,21 +72,57 @@ public class AlgebraLine {
 	 * @param V de vector om te bekijken
 	 * @return GeoPoint met de coordinaten van de projectie
 	 */
-	public static double[] projection(Point p, Point V) {
-		double a = ((double)innerproduct(p, V)) / innerproduct(V, V);
-		double[] r = new double[2];
-		r[0] = a * p.x;
-		r[1] = a * p.y;
-		return r;
+	private double[] projection(Point p) {
+		double[] proj = new double[2];
+		//http://www.codeguru.com/forum/showthread.php?t=194400
+		double r = segmentalInproduct(start, p, start, end) / Math.pow(distance(start, end),2);
+		Log.v(HelloGoogleMaps.TAG, "r: "+r);
+		if (r <= 0) {
+			proj[0] = start.x;
+			proj[1] = start.y;
+		}
+		else if (r >= 1) {
+			proj[0] = end.x;
+			proj[1] = end.y;
+		}
+		else {
+			proj[0] = start.x + r*(end.x - start.x);
+			proj[1] = start.y + r*(end.y - start.y);
+		}
+		return proj;	
 	}
 	
 	/**
-	 * Rekent het inproduct uit van twee GeoPoints (waar ze dus als vectoren worden beschouwd)
-	 * @param p het eerste geopoint
-	 * @param q het tweede geopoint
-	 * @return int het inproduct van de twee geopoints
+	 * Rekent de afstand uit tussen twee punten
+	 * @param p het eerste point
+	 * @param q het tweede point
+	 * @return double de afstand tussen de punten
 	 */
-	private static int innerproduct(Point p, Point q) {
-		return p.y * q.y + p.x * q.x;
+	private static double distance(Point p, Point q) {
+		return Math.sqrt(Math.pow(p.y - q.y,2) + Math.pow(p.x - q.x,2));
 	}
+	
+	/**
+	 * Rekent de afstand uit tussen twee punten, waarvan 1 bestaande uit doubles
+	 * @param p het eerste point
+	 * @param qx het x-coordinaat van het eerste punt
+	 * @param qy het y-coordinaat van het tweede punt
+	 * @return double de afstand tussen de punten
+	 */
+	private static double distance(Point p, double qx, double qy) {
+		return Math.sqrt(Math.pow(p.y - qy,2) + Math.pow(p.x - qx,2));
+	}
+	
+	/**
+	 * Rekent het inproduct uit van twee segmenten
+	 * @param A het eerste point van segment AB
+	 * @param B het tweede point van segment AB
+	 * @param C het eerste point van segment CD
+	 * @param D het tweede point van segment CD
+	 * @return int het inproduct van de twee points
+	 */
+	private static int segmentalInproduct(Point A, Point B, Point C, Point D) {
+		return (A.x - B.x) * (C.x - D.x) + (A.x - B.x) * (C.x - D.x);
+	}
+
 }
