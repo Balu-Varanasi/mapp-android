29b0bc33de85a43ace19ccd4d6b2be811084dbeb
MathijsV <mathijsvos@live.nl>
2011-05-23 17:27:47 +0200

    Wat kleine updatejes aan serversynchronisatie (die nu ineens werkt)

--- src/nl/appcetera/mapp/ServerSync.java
+++ src/nl/appcetera/mapp/ServerSync.java
@@ -1,5 +1,19 @@
 package nl.appcetera.mapp;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
+
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthenticationException;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.auth.DigestScheme;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Handler;
@@ -63,21 +77,56 @@ public class ServerSync implements Runnable
 	 *
 	 */
 	private class DataSyncTask extends AsyncTask<PolygonData, Void, String>
-	{
+	{		
 		/**
 		 * Sync op de achtergrond
 		 */
 		@Override
 		protected String doInBackground(PolygonData... db)
 		{
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpGet httpget = new HttpGet("http://192.168.2.2/MVics/Mappserver/v1/polygons");
+			DigestScheme digestAuth = new DigestScheme();
+			digestAuth.overrideParamter("algorithm", "MD5");
+			digestAuth.overrideParamter("realm", "http://192.168.2.2/MVics/Mappserver/v1/polygons");
+			digestAuth.overrideParamter("nonce", Long.toString(new Random().nextLong(), 36));
+			digestAuth.overrideParamter("qop", "auth");
+			digestAuth.overrideParamter("nc", "0");
+			digestAuth.overrideParamter("cnonce", DigestScheme.createCnonce());
+
 			try
 			{
-				Thread.sleep(20000);
+				Header auth = digestAuth.authenticate(new
+				      UsernamePasswordCredentials("test@example.com", "098f6bcd4621d373cade4e832627b4f6"), httpget);
+				httpget.setHeader(auth);
 			} 
-			catch (InterruptedException e)
+			catch (AuthenticationException e)
 			{
-				e.printStackTrace();
+				return "Authentication failed";
 			}
+
+		    try
+		    {
+		        // Add your data
+		        /*List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+		        nameValuePairs.add(new BasicNameValuePair("id", "12345"));
+		        nameValuePairs.add(new BasicNameValuePair("stringdata", "AndDev is Cool!"));
+		        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));*/
+
+		        // Execute HTTP Post Request
+		        HttpResponse response = httpclient.execute(httpget);
+		        InputStream is = response.getEntity().getContent();
+		        //is.
+		    }
+		    catch (ClientProtocolException e)
+		    {
+		    	e.printStackTrace();
+		        return e.getMessage();
+		    }
+		    catch (IOException e)
+		    {
+		        return e.getMessage();
+		    }
 			
 			return "Ok";
 		}
@@ -92,6 +141,13 @@ public class ServerSync implements Runnable
 			if(result == "Ok")
 			{				
 				CharSequence text = "Sync complete";
+				int duration = Toast.LENGTH_LONG;
+				Toast toast = Toast.makeText(ServerSync.c, text, duration);
+				toast.show();
+			}
+			else
+			{
+				CharSequence text = "Error: " + result;
 				int duration = Toast.LENGTH_LONG;
 				Toast toast = Toast.makeText(ServerSync.c, text, duration);
 				toast.show();
